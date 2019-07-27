162e13932526fbf20f7eb920fd8aba82162c1508
vic30004 <victor_993@msn.com>
2019-07-27 01:06:39 -0700

    added css and questions/answers

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -10,47 +10,47 @@ let results = {
 let allQuestions = [
     {
         id: "question1",
-        q: "something?",
-        a1: "pig",
-        a2: "chicken",
-        a3: "cat",
-        a4: "dog",
-        correctAnswer: "cat",
-        gifCorrect: "https://i.giphy.com/media/WNJVp6owOFjBlO0e8w/giphy.webp",
-        gifIncorrect: "https://i.giphy.com/media/WNJVp6owOFjBlO0e8w/giphy.webp"
+        q: "Who are the author's of the miniseries Deadpool Kills the Marvel Universe?",
+        a1: "Alan Moore and Neil Gaiman",
+        a2: "Cullen Bunn and Dalibor Talajić's",
+        a3: "Warren Ellis and Stan Lee",
+        a4: "Jack Kirby",
+        correctAnswer: "Cullen Bunn and Dalibor Talajić's",
+        gifCorrect: "https://i.giphy.com/media/l0MYDGA3Du1hBR4xG/giphy.webp",
+        gifIncorrect: "https://i.giphy.com/media/yvBAuESRTsETqNFlEl/giphy.webp"
     },
     {
         id: "question2",
-        q: "something?",
-        a1: "lemon",
-        a2: "orange",
-        a3: "tomato",
-        a4: "apple",
-        correctAnswer: "lemon",
-        gifCorrect: "https://i.giphy.com/media/WNJVp6owOFjBlO0e8w/giphy.webp",
-        gifIncorrect: "https://i.giphy.com/media/WNJVp6owOFjBlO0e8w/giphy.webp"
+        q: "Who was the Batman in Flashpoint?",
+        a1: "Bruce Wayne",
+        a2: "The Joker",
+        a3: "Alfred",
+        a4: "Thomas Wayne",
+        correctAnswer: "Thomas Wayne",
+        gifCorrect: "https://i.giphy.com/media/PSByJSyJmNep2/giphy.webp",
+        gifIncorrect: "https://i.giphy.com/media/wt2lezbcFW9qg/giphy.webp"
     },
     {
         id: "question3",
-        q: "something?",
-        a1: "LOL",
-        a2: "Smite",
-        a3: "DOTA",
-        a4: "TFT",
-        correctAnswer: "LOL",
-        gifCorrect: "https://i.giphy.com/media/WNJVp6owOFjBlO0e8w/giphy.webp",
-        gifIncorrect: "https://i.giphy.com/media/WNJVp6owOFjBlO0e8w/giphy.webp"
+        q: "What is the name of the DC MOBA game?",
+        a1: "Infinite Crisis",
+        a2: "DC MOBA",
+        a3: "Justice League",
+        a4: "League of Legends",
+        correctAnswer: "Infinite Crisis",
+        gifCorrect: "https://i.giphy.com/media/3osxYp14leBym7WiVa/giphy.webp",
+        gifIncorrect: "https://i.giphy.com/media/DmWeptBrmLYUU/giphy.webp"
     },
     {
         id: "question4",
-        q: "something?",
-        a1: "basketball",
-        a2: "football",
-        a3: "golf",
-        a4: "tennis",
-        correctAnswer: "golf",
-        gifCorrect: "https://i.giphy.com/media/WNJVp6owOFjBlO0e8w/giphy.webp",
-        gifIncorrect: "https://i.giphy.com/media/WNJVp6owOFjBlO0e8w/giphy.webp"
+        q: "Who is the most powerful Mutant?",
+        a1: "Dark Phoenix",
+        a2: "Professor X",
+        a3: "Santa",
+        a4: "Wolverine",
+        correctAnswer: "Santa",
+        gifCorrect: "https://i.giphy.com/media/EBCzEAdeUvPTDTjbyR/giphy.webp",
+        gifIncorrect: "https://i.giphy.com/media/9LIzdSmzvq8vu/giphy.webp"
     }
 ];
 let questions = $.extend(true, [], allQuestions);
@@ -65,11 +65,15 @@ function questionSelection() {
             }
         }
     }
+    else {
+        endGame();
+    }
 }
+
 
 function createQuestions(question, parentId) {
     let resultElement = "<div class='text-content' id='" + question.id + "'>";
-    resultElement += "<span id='timer'>  </span>"
+    resultElement += "<div id='timer'>  </div>"
     resultElement += "<h3> " + question.q + "</h3>";
     resultElement += "<div class='answers'>"
     resultElement += "<p class='q1'>" + question.a1 + "</p>";
@@ -113,15 +117,15 @@ function createQuestionData(state, question) {
     let dataElement = "<div class='text-content''>";
     switch (state) {
         case "correct":
-            dataElement += "<p>You got it right!</p>";
+            dataElement += "<p class='inform'>You got it right!</p>";
             dataElement += "<img src=" + question.gifCorrect + ">";
             break;
         case "incorrect":
-            dataElement += "<p>Incorrect! The correct answer is " + question.correctAnswer + "</p>";
+            dataElement += "<p class='inform'>Incorrect! The correct answer is " + question.correctAnswer + "</p>";
             dataElement += "<img src=" + question.gifIncorrect + ">";
             break;
         case "outOfTime":
-            dataElement += "<p>You ran out of time! The correct answer is " + question.correctAnswer + "</p>";
+            dataElement += "<p class='inform'>You ran out of time! The correct answer is " + question.correctAnswer + "</p>";
             dataElement += "<img src=" + question.gifIncorrect + ">";
             break;
     }
